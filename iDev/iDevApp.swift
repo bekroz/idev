4e54f18e1c5ec72dd4650a504e974f9d8fa9b486
bekroz <bekrozikoff@gmail.com>
2024-04-13 13:48:46 +0400

    Initial Commit

---
+++ iDev/iDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDevApp.swift
+//  iDev
+//
+//  Created by Bek Roz on 13.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct iDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
